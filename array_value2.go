2fc8d179ef5dc85a383aaf0c7ae6e6f707c50152
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-15 11:47:26 +0530

    print address

---
+++ array_value2.go
@@ -0,0 +1,17 @@
+// Sample program to show how the behavior of the for range and
+// how memory for an array is contiguous.
+package main
+
+import "fmt"
+
+func main() {
+
+	// Declare an array of 5 strings initialized with values.
+	friends := [5]string{"Annie", "Betty", "Charley", "Doug", "Edward"}
+
+	// Iterate over the array displaying the value and
+	// address of each element.
+	for i, v := range friends {
+		fmt.Printf("Value[%s]\tAddress[%p] IndexAddr[%p]\n", v, &v, &friends[i])
+	}
+}
