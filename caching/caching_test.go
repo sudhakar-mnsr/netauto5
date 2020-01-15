823dfec90b827168a5a1f5681df2922120ff4590
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-15 08:04:11 +0530

    one more error case

--- caching/caching_test.go
+++ caching/caching_test.go
@@ -17,3 +17,25 @@ func BenchmarkLinkListTraverse(b *testing.B) {
 
 	fa = a
 }
+
+// Capture the time it takes to perform a column traversal.
+func BenchmarkColumnTraverse(b *testing.B) {
+	var a int
+
+	for i := 0; i < b.N; i++ {
+		a = ColumnTraverse()
+	}
+
+	fa = a
+}
+
+// Capture the time it takes to perform a row traversal.
+func BenchmarkRowTraverse(b *testing.B) {
+	var a int
+
+	for i := 0; i < b.N; i++ {
+		a = RowTraverse()
+	}
+
+	fa = a
+}
