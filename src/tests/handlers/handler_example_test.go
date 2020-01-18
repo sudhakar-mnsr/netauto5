8584553fa8d88fa9a5f9790afb8a0c1336710a0a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-18 22:17:40 +0530

    profile example

---
+++ src/tests/handlers/handler_example_test.go
@@ -0,0 +1,32 @@
+// go test -run ExampleSendJSON
+
+// Sample to show how to write a basic example.
+package handlers_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+)
+
+// ExampleSendJSON provides a basic example example.
+func ExampleSendJSON() {
+	r := httptest.NewRequest("GET", "/sendjson", nil)
+	w := httptest.NewRecorder()
+	http.DefaultServeMux.ServeHTTP(w, r)
+
+	var u struct {
+		Name  string
+		Email string
+	}
+
+	if err := json.NewDecoder(w.Body).Decode(&u); err != nil {
+		log.Println("ERROR:", err)
+	}
+
+	fmt.Println(u)
+	// Output:
+	// {Bill bill@ardanlabs.com}
+}
