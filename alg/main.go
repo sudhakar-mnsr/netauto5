87d9776e4291f89caa94a7b1630010310f649798
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-20 22:02:33 +0530

    example

--- alg/main.go
+++ alg/main.go
@@ -12,3 +12,18 @@ import (
 
 	"github.com/ardanlabs/gotraining/topics/go/algorithms/fun/barber/shop"
 )
+
+func main() {
+	const maxChairs = 10
+	s := shop.Open(maxChairs)
+
+	// Create a goroutine than is constantly, but inconsistently, generating
+	// customers who are entering the shop.
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+
+	fmt.Println("Shutting down shop")
+	s.Close()
+}
