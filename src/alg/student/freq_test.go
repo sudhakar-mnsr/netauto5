c2c6abfcdbb2f556d5283a1efa3497c0faebd4f2
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-21 22:25:51 +0530

    example

--- src/alg/student/freq_test.go
+++ src/alg/student/freq_test.go
@@ -5,3 +5,24 @@ import (
 
 	freq "student/freq"
 )
+
+// go test -run none -bench . -benchtime 3s
+
+const succeed = "\u2713"
+const failed = "\u2717"
+
+func init() {
+	inp = buildText()
+}
+
+func buildText() []string {
+	const n = 100
+	var s []string
+	for i := 0; i < n; i++ {
+		s = append(s, sentence)
+	}
+	for k, v := range out {
+		out[k] = v * n
+	}
+	return s
+}
