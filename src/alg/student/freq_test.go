f87e813fe95a952ccd1b8e02985c075ad05d4e8b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-21 22:26:21 +0530

    example

--- src/alg/student/freq_test.go
+++ src/alg/student/freq_test.go
@@ -26,3 +26,17 @@ func buildText() []string {
 	}
 	return s
 }
+
+var sentence = `The quick brown fox jumps over the lazy dog Stay hungry. Stay
+foolish Keep going. Be all in Boldness be my friend Screw it, let’s do it My
+life is my message Leave no stone unturned Dream big. Pray bigger`
+
+var inp []string
+
+var out = map[rune]int{
+	'T': 1, 'q': 1, 'p': 2, '’': 1, 'i': 11, 'b': 4, 'w': 2, 'j': 1, 'B': 2,
+	'L': 1, 'e': 20, 'v': 2, 'l': 7, ',': 1, 'h': 4, 'u': 5, 'f': 4, 's': 9,
+	'g': 8, 'D': 1, 'P': 1, ' ': 37, 'z': 1, 'd': 5, '.': 3, 'c': 2, 'r': 9,
+	'o': 11, 'm': 5, '\n': 2, 'x': 1, 'y': 8, 'S': 3, 'K': 1, 'k': 1, 'n': 10,
+	't': 8, 'a': 8, 'M': 1,
+}
