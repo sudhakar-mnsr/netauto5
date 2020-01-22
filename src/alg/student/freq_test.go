41d23974764bcf1932d9fff9b751102e6d305099
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-22 22:51:17 +0530

    example

--- src/alg/student/freq_test.go
+++ src/alg/student/freq_test.go
@@ -14,3 +14,15 @@ const failed = "\u2717"
 func init() {
 	inp = buildText()
 }
+
+func buildText() []string {
+	const n = 100
+	var s []string
+	for i := 0; i < n; i++ {
+		s = append(s, sentence)
+	}
+	for k, v := range out {
+		out[k] = v * n
+	}
+	return s
+}
