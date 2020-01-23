7e7386d0577209afa91841178dacfb15032296f3
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-23 23:31:58 +0530

    example

--- src/alg/student/interface/stringer.go
+++ src/alg/student/interface/stringer.go
@@ -24,3 +24,13 @@ func main() {
  s := Stringer(b)
  fmt.Println(s.String())
 }
+
+/*
+COMPILE WITH -S AND SEE BELOW CALL INFO
+0045 (x.go:25) LEAL    s+-24(SP),BX
+0046 (x.go:25) MOVL    4(BX),BP
+0047 (x.go:25) MOVL    BP,(SP)
+0048 (x.go:25) MOVL    (BX),BX
+0049 (x.go:25) MOVL    20(BX),BX
+0050 (x.go:25) CALL    ,BX
+*/
