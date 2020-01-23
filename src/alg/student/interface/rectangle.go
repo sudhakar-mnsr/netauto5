417af298d0b403833835ab73b2c2e9e799d17886
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-23 23:47:46 +0530

    example

--- src/alg/student/interface/rectangle.go
+++ src/alg/student/interface/rectangle.go
@@ -7,12 +7,18 @@ type Rectangle struct {
 	Width, Height float64
 }
 
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
 func main() {
 	var a Rectangle
 	var b = Rectangle{"I'm b.", 10, 20}
 	var c = Rectangle{Height: 12, Width: 14}
+        var d = b.Area()
 
 	Println(a)
 	Println(b)
 	Println(c)
+	Println(d)
 }
