e9f428823cf624713d28b2393a6eb644e3955024
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-24 02:34:24 +0530

    example

--- src/alg/student/interface/multi_method.go
+++ src/alg/student/interface/multi_method.go
@@ -1,3 +1,7 @@
+package main
+
+import "fmt"
+
 // class NamedObj
 type NamedObj struct {
 	Name string
@@ -17,4 +21,12 @@ type Rectangle struct {
 //override method show
 func (r Rectangle) show() {
 	Println("Rectangle ", r.Name) // "r" is "this"
+}
+
+func main() {
+	var a = NamedObj{"Joe"}
+	var b = Rectangle{NamedObj{"Richard"}, 10, 20}
+
+	a.show("Hello")
+	b.show("Hello")
 }
