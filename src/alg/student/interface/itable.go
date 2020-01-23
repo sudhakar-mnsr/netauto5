20e7f4e2e9fb23ec032a071baf25e4d1bdc80f44
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-24 02:54:36 +0530

    example

--- src/alg/student/interface/itable.go
+++ src/alg/student/interface/itable.go
@@ -24,3 +24,33 @@ func (d Dog) Speak() string {
 	return "Woof!"
 }
 
+/*
+class Cat
+  method Speak() string //non-virtual
+     return "Meow!"
+*/
+type Cat struct {
+}
+
+func (c Cat) Speak() string {
+	return "Meow!"
+}
+
+/*
+class Llama
+  method Speak() string //non-virtual
+     return "LaLLamaQueLLama!"
+*/
+type Llama struct {
+}
+
+func (l Llama) Speak() string {
+	return "LaLLamaQueLLama!"
+}
+
+/*
+func main
+  var animals = [ Dog{}, Cat{}, Llama{} ]
+  for each animal in animals
+     print animal.Speak() // method dispatch via jmp-table
+*/
