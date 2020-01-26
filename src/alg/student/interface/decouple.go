657af510f9db1b3402fcf591c0e11bb927c20fdd
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-26 11:45:55 +0530

    example

--- src/alg/student/interface/decouple.go
+++ src/alg/student/interface/decouple.go
@@ -19,3 +19,22 @@ func (d *data) setAge(age int) {
 	d.age = age
 	fmt.Println(d.name, "Is Age", d.age)
 }
+
+func main() {
+
+	// Declare a variable of type data.
+	d := data{
+		name: "Bill",
+	}
+
+	fmt.Println("Proper Calls to Methods:")
+
+	// How we actually call methods in Go.
+	d.displayName()
+	d.setAge(45)
+
+	fmt.Println("\nWhat the Compiler is Doing:")
+
+	// This is what Go is doing underneath.
+	// data.displayName(d)
+	// (*data).setAge(&d, 45)
