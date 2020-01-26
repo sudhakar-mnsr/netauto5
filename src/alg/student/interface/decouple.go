5293ba486fb816d59635185635cec398ad276e50
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-26 11:44:34 +0530

    example

---
+++ src/alg/student/interface/decouple.go
@@ -0,0 +1,15 @@
+// Sample program to show how to declare function variables.
+package main
+
+import "fmt"
+
+// data is a struct to bind methods to.
+type data struct {
+	name string
+	age  int
+}
+
+// displayName provides a pretty print view of the name.
+func (d data) displayName() {
+	fmt.Println("My Name Is", d.name)
+}
