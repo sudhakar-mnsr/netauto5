be902c41af2d811da0853906eda42f7a65a9c600
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-27 12:16:48 +0530

    example

--- src/data_behavior/embedd.go
+++ src/data_behavior/embedd.go
@@ -16,3 +16,25 @@ type user struct {
 func (u *user) notify() {
    fmt.Printf("Sending user email to %s<%s>\n", u.name, u.email)
 }
+
+// admin represents an amdin user with privileges
+type admin struct {
+   user // Embedded type
+   level string
+}
+
+func main() {
+   // Create an admin user
+   ad := admin{
+      user: user{
+         name: "John Smith",
+         email: "john@yahoo.com",
+      },
+      level: "super",
+   }     
+   
+   // we can access the inner types method directly
+   ad.user.notify()
+   // The inner types method is promoted
+   ad.notify()
+}
