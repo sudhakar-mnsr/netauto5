31fe18898ef2979598550cc57b9ceb8e1e7234af
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-27 12:57:45 +0530

    example

--- src/data_behavior/embedd_ambiguity.go
+++ src/data_behavior/embedd_ambiguity.go
@@ -21,3 +21,38 @@ func (u *user) notify() {
    fmt.Printf("Sending user email to %s,%s>\n", u.name, u.email)
 }
 
+// admin represents an admin user with privileges
+type admin struct {
+   user 
+   level string
+}
+
+// notify implements a method notifies admins
+// of different events.
+func (a *admin) notify() {
+   fmt.Printf("sending admin email to %s<%s>\n", a.name, a.email)
+}
+
+func main() {
+// Create an admin user
+ad := admin{
+   user: user{
+      name: "John Smith",
+      email: "john@yahoo.com",
+   },
+   level: "super",
+}
+
+// Send the admin user a notification
+// The embedded inner types implementation of the 
+// interface is NOT "promoted" to the outer type.
+sendNotification(&ad)
+ad.user.notify()
+ad.notify()
+}
+
+// sendNotification accepts values that implement the notifier
+// interface and sends notifications.
+func sendNotification(n notifier) {
+   n.notify()
+} 
