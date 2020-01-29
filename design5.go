a0ca8ac15c334d5a556f00dd74a9f0803b3f07ac
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-29 09:55:28 +0530

    example

---
+++ design5.go
@@ -0,0 +1,65 @@
+// Sample program demonstrating being more precise with API design.
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// =============================================================================
+
+// Data is the structure of the data we are copying.
+type Data struct {
+	Line string
+}
+
+// =============================================================================
+
+// Puller declares behavior for pulling data
+type Puller interface {
+   Pull(d *Data) error
+}
+// Storer declares behavior for storing data
+type Storer interface {
+   Store(d *Data) error
+}
+
+// =============================================================================
+// Xenia is a system we need to pull data from.
+type Xenia struct {
+   Host string
+   Timeout time.Duration
+}
+
+// PUll knows how to pull data out of Xenia
+func (*Xenia) Pull(d *Data) error {
+   switch rand.Intn(10) {
+   case 1, 9:
+      return io.EOF
+   case 5:
+      return errors.New("Error reading data from Xenia")
+   default:
+      d.Line = "Data"
+      fmt.Println("In:", d.Line)
+      return nil
+   }
+}
+
+// Pillar is a system we need to store data into.
+type Pillar struct {
+   Host string
+   Timeout time.Duration
+}
+
+// Store knows how to store data into Pillar.
+func (*Pillar) Store(d *Data) error {
+   fmt.Println("Out:", d.Line)
+   return nil
+}
