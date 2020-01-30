3060d15965bb6fd5777b3acf94ed5a52e25cae4e
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-30 10:57:26 +0530

    example

---
+++ src/error_handle/reason.go
@@ -0,0 +1,21 @@
+// Sample program to show see if the class can find the bug.
+
+package main
+import (
+   "fmt"
+   "log"
+)
+
+// customError is just an empty struct
+type customError struct{}
+
+// Error implements the error interface
+func (c *customError) Error() string {
+   return "Find the bug."
+}
+
+// fail return nil values for both return types.
+func fail() ([]byte, *customError) {
+   return nil, nil
+}
+
