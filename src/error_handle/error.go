f66c34eedb47a3313af3a1eb51007d5b9c7e2b14
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-30 11:22:03 +0530

    example

--- src/error_handle/error.go
+++ src/error_handle/error.go
@@ -16,3 +16,10 @@ func fail() ([]byte, *customError) {
    return nil, nil
 }
 
+func main() {
+   var err error
+   if _, err = fail(); err != nil {
+      log.Fatal("Why did this fail?")
+   }
+   log.Println("No Error")
+}
